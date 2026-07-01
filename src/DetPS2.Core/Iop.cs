6faf944c35b26b54acc16fee8ef3bc996a39069a
RazmanianDVL <108887694+RazmanianDVL@users.noreply.github.com>
2026-07-01 17:38:42 -0500

    Continue IOP development - expanding instruction set.

--- src/DetPS2.Core/Iop.cs
+++ src/DetPS2.Core/Iop.cs
@@ -47,7 +47,7 @@ public sealed class Iop
     {
         if (!Running) return;
 
-        for (int i = 0; i < 65536 && Running; i++) // Extremely high throughput
+        for (int i = 0; i < 131072 && Running; i++) // Extremely high throughput
         {
             uint opcode = _memory.Read32(PC);
             ExecuteInstruction(opcode);
