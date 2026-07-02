640e1d359d33f4ab13468de211c2a8c1721bd27b
RazmanianDVL <108887694+RazmanianDVL@users.noreply.github.com>
2026-07-01 19:45:30 -0500

    Add more real instructions to IOP.

--- src/DetPS2.Core/Iop.cs
+++ src/DetPS2.Core/Iop.cs
@@ -50,7 +50,7 @@ public sealed class Iop
     {
         if (!Running) return;
 
-        for (int i = 0; i < 8388608 && Running; i++)
+        for (int i = 0; i < 16777216 && Running; i++)
         {
             uint opcode = _memory.Read32(PC);
             ExecuteInstruction(opcode);
