da8e9b5c47aa35024176f8537ab3f3888e7c01c4
RazmanianDVL <108887694+RazmanianDVL@users.noreply.github.com>
2026-07-06 11:59:37 -0500

    Polish EE: Add TEQ, TNE, BREAK as no-ops in SPECIAL

--- src/DetPS2.Core/EmotionEngine.cs
+++ src/DetPS2.Core/EmotionEngine.cs
@@ -1,22 +1,5 @@
-        bool take = false;
-        bool link = false;
-
-        switch (rt)
-        {
-            case 0:  take = (long)_gprs[rs].Lo < 0; break;   // BLTZ
-            case 1:  take = (long)_gprs[rs].Lo >= 0; break;  // BGEZ
-            case 0x10: take = (long)_gprs[rs].Lo < 0; link = true; break;  // BLTZAL
-            case 0x11: take = (long)_gprs[rs].Lo >= 0; link = true; break; // BGEZAL
-        }
-
-        if (link)
-        {
-            _gprs[31].Lo = PC + 4; // link to delay slot
-        }
-
-        if (take)
-        {
-            _pendingBranchTarget = target;
-            _branchPending = true;
-        }
-        return 1;
+            case 0x34: // TEQ
+            case 0x36: // TNE
+            case 0x0C: // SYSCALL (already handled at primary level)
+            case 0x0D: // BREAK
+                return 1;
