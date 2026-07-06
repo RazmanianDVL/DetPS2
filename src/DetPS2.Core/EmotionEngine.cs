d4f67baf41ac693fc8c2077617c128eb70fd83c3
RazmanianDVL <108887694+RazmanianDVL@users.noreply.github.com>
2026-07-06 11:59:28 -0500

    Polish EE: Add SYNC, MOVZ, MOVN instructions

--- src/DetPS2.Core/EmotionEngine.cs
+++ src/DetPS2.Core/EmotionEngine.cs
@@ -1,27 +1,10 @@
-    private int ExecuteLwc1(uint opcode)
-    {
-        uint rs = (opcode >> 21) & 0x1F;
-        uint rt = (opcode >> 16) & 0x1F;
-        short offset = (short)(opcode & 0xFFFF);
-        ulong addr = _gprs[rs].Lo + (ulong)(long)offset;
-        uint bits = _memory.Read32(addr);
-        _fprs[rt] = BitConverter.Int32BitsToSingle((int)bits);
-        return 1;
-    }
+            case 0x0F: // SYNC
+                return 1;
 
-    private int ExecuteSwc1(uint opcode)
-    {
-        uint rs = (opcode >> 21) & 0x1F;
-        uint rt = (opcode >> 16) & 0x1F;
-        short offset = (short)(opcode & 0xFFFF);
-        ulong addr = _gprs[rs].Lo + (ulong)(long)offset;
-        uint bits = (uint)BitConverter.SingleToInt32Bits(_fprs[rt]);
-        _memory.Write32(addr, bits);
-        return 1;
-    }
+            case 0x0B: // MOVN
+                if (rd != 0 && _gprs[rt].Lo != 0) _gprs[rd].Lo = _gprs[rs].Lo;
+                return 1;
 
-    private int HandleUnknown(uint opcode)
-    {
-        Console.WriteLine($"[EE] Unknown opcode 0x{opcode:X8} at PC=0x{PC:X8}");
-        return 1;
-    }
+            case 0x0A: // MOVZ
+                if (rd != 0 && _gprs[rt].Lo == 0) _gprs[rd].Lo = _gprs[rs].Lo;
+                return 1;
